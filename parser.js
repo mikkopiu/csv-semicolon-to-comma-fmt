49b81ba4db1364e06e4add5d78dc17868140d8b9
Mikko Piuhola <mikko.piuhola@metropolia.fi>
2015-02-27 21:03:43 +0200

    Moved parsing into separate function, commenting

--- parser.js
+++ parser.js
@@ -1,21 +1,42 @@
 var fs = require('fs');
 var through = require('through');
 
+// File input & output given as arguments
 var from = process.argv[2];
 var to = process.argv[3];
 
+// Raise the buffer limits
 var readOpts = {highWaterMark: Math.pow(2,16)};
 var writeOpts = {highWaterMark: Math.pow(2,16)};
 
+// Create file streams
 var source = fs.createReadStream(__dirname + '/' + from, readOpts);
 var destiny = fs.createWriteStream(__dirname + '/' + to, writeOpts);
 
+// Pipe data from read-stream to write- and end-functions
 source.pipe(through(write, end));
 
+/**
+ * Write buffer data to file
+ * @param {Buffer} buf Buffer of a readable stream's data
+ */
 function write (buf) {
-    destiny.write(buf.toString().replace(/,/g, ';'));
+    destiny.write(parse(buf));
 }
 
+/**
+ * Parse buffer data.
+ * Replaces semicolons with commas.
+ * @param {Buffer} buf Buffer of a readable stream's data
+ * @return {String} Parsed Buffer as a String
+ */
+function parse (buf) {
+    return buf.toString().replace(/;/g, ',');
+}
+
+/**
+ * Finished writing
+ */
 function end () {
     console.log('Finished parsing');
 }
