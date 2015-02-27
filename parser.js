1b94cd221aa8ca9c5a27b9d8420588973b84f7d3
Mikko Piuhola <mikko.piuhola@metropolia.fi>
2015-02-27 20:54:31 +0200

    Complete parser overhaul, updated dependencies

--- parser.js
+++ parser.js
@@ -1,70 +1,21 @@
 var fs = require('fs');
+var through = require('through');
 
-var fileInput = process.argv[2];
-var fileOutput = process.argv[3];
+var from = process.argv[2];
+var to = process.argv[3];
 
-var readStream = fs.createReadStream(__dirname + '/' + fileInput, {flags: 'r'});
-var writeStream = fs.createWriteStream(__dirname + '/' + fileOutput);
-var buf = '';
-var writeBuf = '';
+var readOpts = {highWaterMark: Math.pow(2,16)};
+var writeOpts = {highWaterMark: Math.pow(2,16)};
 
-readStream.on('data', function (data) {
-    buf += data.toString(); // When data is read, set it into a string buffer
-    pump(); // Process the buffer
-});
+var source = fs.createReadStream(__dirname + '/' + from, readOpts);
+var destiny = fs.createWriteStream(__dirname + '/' + to, writeOpts);
 
-/**
- * Pump buffer data to parser
- */
-function pump() {
-    var pos;
+source.pipe(through(write, end));
 
-    // Keep going while there's a newline somewhere in the buffer
-    while ((pos = buf.indexOf('\n')) >= 0) {
-        // If there's more than one newline in a row, the buffer will now start with a newline
-        if (pos === 0) {
-            buf = buf.slice(1); // Discard it
-            continue; // So that the next iteration will start with data
-        }
-
-        parse(buf.slice(0, pos)); // Hand off the line
-        buf = buf.slice(pos + 1); // Slice the processed data off the buffer
-    }
+function write (buf) {
+    destiny.write(buf.toString().replace(/,/g, ';'));
 }
 
-/**
- * Parse a line string
- * Calls write when parsed
- * @param {String} line Line to parse
- */
-function parse (line) {
-    if (line[line.length - 1] == '\r') {
-        line = line.substr(0, line.length - 1); // Discard CD (0x0D)
-    }
-
-    // Ignore empty lines
-    if (line.length > 0) {
-        var parsedLine = line.replace(/,/g, ';'); // Parse the line
-        writeBuf += parsedLine + '\n';
-        write();
-    }
-}
-
-/**
- * Write to file
- */
-function write() {
-    var pos;
-
-    // Keep going while there's a newline somewhere in the buffer
-    while ((pos = writeBuf.indexOf('\n')) >= 0) {
-        // If there's more than one newline in a row, the buffer will now start with a newline
-        if (pos === 0) {
-            writeBuf = writeBuf.slice(1); // Discard it
-            continue; // So that the next iteration will start with data
-        }
-
-        writeStream.write(writeBuf.slice(0, pos)); // Hand off the line
-        writeBuf = writeBuf.slice(pos + 1); // Slice the processed data off the buffer
-    }
+function end () {
+    console.log('Finished parsing');
 }
